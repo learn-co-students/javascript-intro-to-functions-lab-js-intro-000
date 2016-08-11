c7b27acd63b103d7ebbf5741199afabfb849362a
Katarina <kepasinsky@gmail.com>
2016-08-11 19:40:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase (string))
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase (string))
+}
+
+function sayHiToGrandma(string) {
+  if ()
+}
