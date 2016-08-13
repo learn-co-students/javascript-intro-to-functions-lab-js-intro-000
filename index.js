2eae186166da395361bea705e8c50b6a31a01717
maxgrok <maxgrok2011@gmail.com>
2016-08-13 18:29:18 -0400

    Done.

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   if (string === string.toLowerCase()){
-    returns "I can\'t hear you!";
+    return "I can\'t hear you!";
   }
   else if (string === string.toUpperCase()){
-    returns "YES INDEED!";
+    return "YES INDEED!";
   }
   else if (string === "I love you, Grandma."){
-    returns "I love you, too.";
+    return "I love you, too.";
   }
 }
