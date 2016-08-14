a76c1c3c7f69be4891efd06f9cf814efd9e977d6
Oksana Tiurina <oksana.tyurina@gmail.com>
2016-08-14 22:48:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
 
 function logShout(string) {
-  return string.toUpperCase();
   console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
 }
 
 function sayHiToGrandma(string) {
@@ -18,7 +21,20 @@ function sayHiToGrandma(string) {
   if (string === string.toUpperCase()) {
     return "YES INDEED!";
   }
-    if (string === "I love you, Grandma.") {
+  if (string === "I love you, Grandma.") {
     return "I love you, too.";
   }
 }
+/*
+function sayHiToGrandma(string) {
+  if (string === whisper(string)){
+    return "I can\'t hear you!";
+  }
+  else if (string === shout(string)){
+    return "YES INDEED!";
+  }
+  else if (string === "I love you, Grandma."){
+    return "I love you, too.";
+  }
+}
+*/
