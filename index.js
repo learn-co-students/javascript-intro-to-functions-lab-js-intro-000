2895ac6f8ef271728fc51fc93224e83d583f0227
Oksana Tiurina <oksana.tyurina@gmail.com>
2016-08-14 21:50:42 +0000

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
+function logShout(string){
+  console.log(string.toUpperCase());
+}
+
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return "I can\'t hear you!";
