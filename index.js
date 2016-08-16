29c4f0f46f3808130558a29a65f4391a957779bc
Allison Eng <allisonmichelle.bk@gmail.com>
2016-08-16 17:52:52 +0000

    Done.

--- index.js
+++ index.js
@@ -3,5 +3,29 @@ function shout(string) {
 }
 
 function whisper(string) {
-  return string.toLowerCase
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  string = string.toUpperCase();
+  console.log(string);
+}
+
+function logWhisper(string) {
+  string = string.toLowerCase();
+  console.log(string)
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase())
+  {
+return "I can\'t hear you!"
+} else if
+  (string === string.toUpperCase())
+  {
+return "YES INDEED!"
+} else {
+  (string === "I love you, Grandma.")
+return "I love you, too."
+}
 }
