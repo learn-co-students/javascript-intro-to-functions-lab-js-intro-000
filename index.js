e2d040c8ce2663bd19d617da9cd1798d1bd26982
James Renna <rennajd@gmail.com>
2016-08-18 21:46:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string) {
+  return string.toUpperCase("hello")
+}
+
+function whisper(string) {
+  return string.toLowerCase("HELLO")
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase("hello"))
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase("HELLO"))
+}
+
+function sayHiToGrandma(string) {
+  if ("hello") {
+    return "I can't hear you!"}("hello");
+  if ("HELLO") {
+    return "YES INDEED!"}("HELLO");
+  if ("I love you, Grandma.") {
+    return "I love you, too."}("I love you, Grandma.");
+}
