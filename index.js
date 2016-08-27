ef1abc50c89a12dab24d9fd873b3b31c6ae7da1e
Devy Js <devyjs77@gmail.com>
2016-08-27 04:53:40 +0000

    Done.

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  var lowercase = (string)
-  if lowercase.toLowerCase() === lowercase
-  return "I can\'t hear you!"
+  if (string.toLowerCase() === string)
+  return ("I can't hear you!")
+  if (string.toUpperCase() === string)
+  return ("YES INDEED!")
+  if (string = ("I love you, Grandma."))
+  return ("I love you, too.")
 }
