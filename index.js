b148790ad9e8ba6e0937b0271e9e5d8620129e00
TJ Carney <tjcarney@stny.rr.com>
2016-09-02 18:56:39 +0000

    Done.

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if(string === string.toLowerCase())
   {return "I can\'t hear you!";}
-}
-function sayHiToGrandma(string) {
   if(string === string.toUpperCase())
   {return "YES INDEED!";}
+  if(string === "I love you, Grandma.")
+  {return "I love you, too."}
 }
