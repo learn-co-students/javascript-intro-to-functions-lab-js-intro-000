3fc1c778b390084892adccf97c7be19b37401dc7
Rishi Shrivastava <rshrivastava@gmail.com>
2016-09-26 19:45:53 +0000

    Done.

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   } else if(string === string.toUpperCase()) {
     return "YES INDEED!"
-  } else if(string === "I love you, Grandma.") {
+  } else {
     return "I love you, too."
   }
 }
