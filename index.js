dc67c6eab89024e504212397d6fccd007078dfd5
Todd Peterman <xxxtoad24@gmail.com>
2016-09-28 00:01:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+function shout(Hello){
+  return(Hello.toUpperCase('HELLO!'));
+}
+function whisper(Hello){
+  return(Hello.toLowerCase('hello!'));
+}
+function logShout(hello){
+  console.log(hello.toUpperCase('HELLO!'))
+}
+function logWhisper(What){
+  console.log(What.toLowerCase('what!'))
+}
