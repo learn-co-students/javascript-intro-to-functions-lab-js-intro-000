4db4616f37fc2bd0f5e85b09bc77db4a68366519
Jannelle Page <jannelle.page@gmail.com>
2016-10-20 19:09:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+function shout(string) {
+    return 'HELLO'.toUpperCase()
+}
+function whisper(string) {
+  return 'hello'.toLowerCase()
+}
+function sayHiToGrandma(string) {
+  return 'sayhitograndma'.toLowerCase()
+}
+function sayHiToGrandma(string) {
+  console.log() 'I cant hear you!'.toLowerCase()
+}
