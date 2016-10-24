bec28f96e44b6dc43d88e64a965d5af1fdac3cff
Ines Pucker <ines.pucker@gmx.de>
2016-10-24 14:18:27 +0000

    Done.

--- index.js
+++ index.js
@@ -12,3 +12,13 @@ console.log(string.toUpperCase())
 function logWhisper (string) {
 console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string){
+if (string.toLowerCase () === string){
+return ("I can\'t hear you!")}
+else if(string.toUpperCase () === string){
+return ("YES INDEED!")}
+else if
+(string ==="I love you, Grandma."){
+return ("I love you, too.")}
+}
