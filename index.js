8b1bb5681a9b3a37801f22c9c45b32945fd60637
David Clayton <davidclayton1989@hotmail.com>
2016-10-27 17:13:36 +0000

    Done.

--- index.js
+++ index.js
@@ -19,6 +19,16 @@ function logWhisper(string) {
 
 
 function sayHiToGrandma(string) {
-  if string === lowerCasereturn 
-  return ("I can't hear you!")
+  if (string .toLowerCase() === string) {
+  return "I can't hear you!"
+}
+
+if (string .toUpperCase() === string) {
+  return "YES INDEED!"
+}
+
+if (string === "I love you, Grandma.") {
+  return "I love you, too."
+}
+
 }
