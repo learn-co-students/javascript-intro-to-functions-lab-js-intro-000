92b38fd77a37f0238d61bfbeb5cfd345008ae222
Ron Belmont <ronnybelmontes@aim.com>
2016-10-29 19:14:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log('Hello'.toUpperCase()) }
+
+  function logWhisper(string) {
+    console.log('Hello'.toLowerCase()) }
+
+    function sayHiToGrandma(string) {
+      console.log("I can't hear you!")
+     }
