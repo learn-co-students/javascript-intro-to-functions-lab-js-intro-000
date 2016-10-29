6c7acc62c33bd2f62065c974699a3449d7de4c70
Ron Belmont <ronnybelmontes@aim.com>
2016-10-29 21:11:12 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function logShout(string) {
 
   function logWhisper(string) {
     console.log('Hello'.toLowerCase()) }
+
     function sayHiToGrandma (string) { if
        (string.toUpperCase() >= string)
        return ("YES INDEED!")
