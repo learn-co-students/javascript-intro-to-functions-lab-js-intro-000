be82cf7221d2a069b9bca4a74ebd5283bbe80387
Amit Singh <amit_nyc@live.com>
2016-11-02 19:24:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function shout(string){
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logshout(string){
+  logshout('hello')
+}
+  console.log (string.toUpperCase())
+}
