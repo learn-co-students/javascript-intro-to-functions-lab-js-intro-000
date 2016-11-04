0767580b8231c86f7f0bd1b24b1b78aec2da958b
Steven Grimley <sgrimley@sanfordguide.com>
2016-11-04 15:18:11 +0000

    Done.

--- index.js
+++ index.js
@@ -6,14 +6,27 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logshout(hello){
-  return ('hello')
-  {
-          console.log ('hello'.toUpperCase())
-}}
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
 
-logShout('HELLO')
+function logWhisper (string) {
+  console.log(string.toLowerCase())
+}
 
+function sayHiToGrandma(string) {
+  if (string === 'I love you, Grandma.') {
+    return 'I love you, too.'
+  }
+
+  else if (string === string.toLowerCase()) {
+    return 'I can\'t hear you!'
+  }
+
+  else if (string === string.toUpperCase()) {
+    return 'YES INDEED!'
+  }
+}
 
 
 
