ebffc00a13121ca85f5af6c45699e431d1334804
Anna Brakowska <anna.brakowska91@gmail.com>
2016-11-08 04:18:16 +0000

    Done.

--- index.js
+++ index.js
@@ -16,7 +16,15 @@ function logWhisper (string) {
 }
 
 function sayHiToGrandma (string) {
-  if (string === string.toLowerCase() {
+  if (string === string.toLowerCase()) {
     return  "I can\'t hear you!"
+  } else if (string === string.toUpperCase()) {
+    return 'YES INDEED!'
+  }
+}
+
+function sayHiToGrandma(string){
+  if(string==="I love you, Grandma.") {
+    return "I love you, too."
   }
 }
