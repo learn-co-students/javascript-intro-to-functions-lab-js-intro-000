f2dccea5d1207ae485b6775bf3650decfc90eb1b
devin hanaway <devinh11@gmail.com>
2016-11-17 19:11:07 +0000

    Done.

--- index.js
+++ index.js
@@ -21,3 +21,9 @@ function sayHiToGrandma(string) {
       return "I can't hear you!";
     }
 }
+
+function sayHiToGrandma(string) {
+  if(string === string.toUpperCase()){
+    return "YES INDEED!";
+  }
+}
