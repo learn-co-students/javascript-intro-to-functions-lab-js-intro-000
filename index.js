6a4ec0539acaa4482c5029a9b10ddb8c5a786889
Aaron R. Trevino <aaronrtrevino12@gmail.com>
2016-11-24 21:46:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log ('Hello'.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log ('Hello'.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  var uppercase = "HELLO"
+  var lowercase = "hello"
+  if (string === lowercase)
+  return 'I can\'t hear you!'
+  else if (string === uppercase)
+  return 'YES INDEED!'
+  else (string === "I love you, Grandma.")
+  return 'I love you, too.'
+}
