7058e2db112c94ac500a49ab838a143c89323288
Runnan Zhong <r.tina.zhong@gmail.com>
2016-12-27 18:41:59 +0000

    Done.

--- index.js
+++ index.js
@@ -15,7 +15,15 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if string.toUpperCase() ? {
-  return string.toUpperCase()
-  };
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!";
+  }
+
+  if (string === string.toUpperCase()) {
+    return "YES INDEED!";
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
