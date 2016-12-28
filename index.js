4ec27541f66a2a84594669e05c914a7a1d66d426
Jessica Ibbotson <jessicaibb@gmail.com>
2016-12-28 03:45:23 +0000

    Done.

--- index.js
+++ index.js
@@ -15,18 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string == string.toLowerCase()); {
+  if (string.toLowerCase() === string) {
     return "I can't hear you!"
-  }
-}
-function sayHiToGrandma(string) {
-  if (string == string.toUpperCase()); {
+  else if (string.toUpperCase() === string) {
     return "YES INDEED!"
-  }
-}
-
-function sayHiToGrandma(string) {
-  if (string === "I love you, Grandma.") {
+  else if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
 }
