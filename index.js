0075b0cb551f3be3b0d850b3d2f63a94647c8ae8
Daran Porter <postmaster211@hotmail.com>
2016-12-30 15:15:57 +0000

    Done.

--- index.js
+++ index.js
@@ -19,7 +19,20 @@ function sayHiToGrandma(string) {
     return ("I can't hear you!")
 }
 }
+function shout(string) {
+  return string.toUpperCase()
+}
 
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
 // function sayHiToGrandma(string) {
 //   if (string.toUpperCase()) {
 //     return ("Yes Indeed!")
