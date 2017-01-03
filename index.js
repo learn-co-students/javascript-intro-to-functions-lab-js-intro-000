87e1d93b47ac71a9efc0d3896b0e2fda20dd0603
Aleksei Sinegubovskii <nysqueezzze@gmail.com>
2017-01-03 16:07:11 +0000

    Done.

--- index.js
+++ index.js
@@ -4,10 +4,27 @@ function shout(string) {
 function whisper(string){
   return string.toLowerCase()
 }
-function logShout(string) {
-return console.log()
+function logWhisper(string) {
+var string='hello'
+ console.log(string)
 }
-function logWhisper(string){
-  string.toUpperCase()
-  console.log()
+function logShout(string) {
+  var string='HELLO'
+  console.log(string)
+}
+function sayHiToGrandma(string) {
+switch (string) {
+  case 'hello': return("I can\'t hear you!")
+
+    break;
+  case 'HELLO': return ("YES INDEED!")
+
+    break;
+    case 'I love you, Grandma.':return("I love you, too.")
+    break;
+
+  default: break
+
+}
+
 }
