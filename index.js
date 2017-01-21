7107ce26ca1db31a47153996905a4397a1dbc486
Andreea Glavan <andreeaglavan1@gmail.com>
2017-01-21 10:08:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string){
+  return string.toUpperCase
+}
+
+function whisper(string){
+  return string.toLowerCase
+}
+
+function logShout(string){
+  return console.log().toUpperCase
+}
+
+function logWhisper(string){
+  return console.log().toLowerCase
+}
+
+fucntion sayHiToGrandma(string){
+  if(string==lowercase){
+    return "I can't hear you!"
+  }
+  if(string==uppercase){
+    return "YES INDEED!"
+  }
+  if(string=="I love you, Grandma."){
+    return "I love you, too."
+  }
+}
