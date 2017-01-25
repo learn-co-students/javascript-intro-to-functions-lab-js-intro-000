31e71c56127238c16c15a7f4888ba6d924101cf5
Bryan Vicente <popetomato@gmail.com>
2017-01-25 12:39:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log ('HELLO')
+  var uppercase = 'HELLO'
+  uppercase.toUpperCase() === uppercase
+}
+
+function logWhisper(string) {
+  console.log ('hello')
+  var lowercase = 'hello'
+  lowercase.toLowerCase()
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+   return "I can\'t hear you!";
+ } else if (string.toUpperCase() === string) {
+   return "YES INDEED!";
+ } else if (string === "I love you, Grandma.") {
+   return "I love you, too.";
+ }
+};
