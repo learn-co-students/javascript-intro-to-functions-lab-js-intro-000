e6b82f6b825d2254523e0ace8a923a76db6286f5
Heather You <heatherhyou@gmail.com>
2017-01-26 20:15:30 +0000

    Done.

--- index.js
+++ index.js
@@ -5,22 +5,19 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-  console.log('HELLO')
+  console.log(string.toUpperCase())
 }
 function logWhisper(string) {
-  console.log('hello')
+  console.log(string.toLowerCase())
 }
-var uppercase = "HELLO!"
-uppercase.toUpperCase() === uppercase
-var lowercase = 'hello!'
-lowercase.toLowerCase() === lowercase
-
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase()) {
-    return "I can\'t hear you!";
-  } else if (string === string.toUpperCase()) {
-    return "YES INDEED!";
-  } else if (string === "I love you, Grandma.") {
-    return "I love you, too.";
+  if (string.toLowerCase() === string) {
+    return "I can\'t hear you!"
+  }
+  else if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too."
   }
 }
