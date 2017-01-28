b9e64cf8dfcd8336c0e48df4505322f9cf48af94
Duane Martinez <globalmegahypercompunet@gmail.com>
2017-01-28 07:04:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(string)
+{
+  return string.toUpperCase()
+}
+
+function whisper(string)
+{
+  return string.toLowerCase()
+}
+
+function logShout(string)
+{
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string)
+{
+   console.log(string.toLowerCase())
+}
+
+function shout(string)
+{
+  return string.toUpperCase()
+}
+
+function sayHiToGrandma(string)
+{
+  if
+  {return "YES INDEED!"}
+
+}
