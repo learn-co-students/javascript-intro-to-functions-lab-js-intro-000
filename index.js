3608e71bd2cc29c6d2886d9ba9ab4d22df55a923
Alexandra Latimer <alexmlatimer@gmail.com>
2017-02-01 19:48:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function shout(string) {
+  return string.toUpperCase('hello')
+}
+
+function whisper(string) {
+  return string.toLowerCase('hello')
+}
+
+function logShout(string) {
+ console.log.toUpperCase('hello')
+}
