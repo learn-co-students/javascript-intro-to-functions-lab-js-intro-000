04fb82575ada9e6bbd60ba7732ccb376d5a9c4c1
Alexandra Latimer <alexmlatimer@gmail.com>
2017-02-03 18:01:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 function shout(string) {
-  return string.toUpperCase('hello')
+  return string.toUpperCase()
 }
 
-function whisper(string) {
-  return string.toLowerCase('hello')
+function whisper(string){
+  return string.toLowerCase()
 }
 
-function logShout(string) {
- console.log ('hello'.toUpperCase)
+function logWhisper(string) {
+  console.log(string.toLowerCase())
 }
 
+function logShout(input_string) {
+ //console.log("hello".toUpperCase())
+ console.log(input_string.toUpperCase())
+}
+
+// sayHiToGrandma('hi grandma');
 function sayHiToGrandma(string) {
-  if (`string`("hello".toLowerCase)) {
-  return "I can\'t hear you!"
+  // if(string.toLowercase()) does this exist?
+  // if (string === string.toLowerCase()) 'hello grandma' === 'HELLO GRANDMA'
+  if (string === whisper(string)){
+    return "I can\'t hear you!"
   }
-}
-else if { `string`("hello".toUpperCase)
-  return " YES INDEED!"
+  else if (string === shout(string)) {
+    return "YES INDEED!"
+  }
+  else if (string === "I love you, Grandma."){
+    return "I love you, too."
+  }
 }
