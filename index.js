5abb4bb10b4df2fb411d9bfea5969821ad30dd1c
Enrico De Santis <ricodesantis@gmail.com>
2017-02-16 17:18:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(string){
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log('HELLO')
+}
+function logWhisper(string){
+  console.log('hello')
+}
+function sayHiToGrandma(string) {
+return "I can't hear you!"; string.toLowerCase()
+else
+"Yes Indeed!"; string.toUpperCase()
+else
+"I love you, too."; "I love you, Grandma."
+}
