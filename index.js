1481d191fdfb3020a645a472b537c776ab5ae197
Dahlia Ma <ma.dahlia25@gmail.com>
2017-02-18 05:16:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log.toUpperCase('string')
+}
+function logWhisper(string) {
+  console.log.toLowerCase('string')
+}
+function sayHiToGrandma('hello') {
+  return string.toLowerCase("I can't hear you!")
+}
+function sayHiToGrandma('HELLO') {
+  return string.toUpperCase("YES INDEED!")
+}
+function sayHiToGrandma("I love you, Grandma.") {
+  return "I love you, too."
+}
