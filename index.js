4a71ef65c1fc9c66953cacea0eba3ce9294f47e3
Sasha Bennet <sasha@babypterodactyl.com>
2017-02-26 03:39:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,41 +1,32 @@
+
 function shout(string) {
   return string.toUpperCase()
 }
-shout('hello!');
 
 function whisper(string) {
   return string.toLowerCase()
 }
-whisper('HELLO!');
 
 function logShout(string) {
   console.log(string.toUpperCase())
 }
 
-logShout('hello!');
-
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-logWhisper('HELLO');
-
-
-
 function sayHiToGrandma(string) {
-  if (string == string.toLowerCase){
-    return "I can\'t hear you!";
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
   }
-}
 
-sayHiToGrandma("hello");
-
-
-
-function sayHiToGrandma(string) {
-  if (string == string.toUpperCase){
-    return "YES INDEED!";
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
   }
-}
 
-sayHiToGrandma("HELLO");
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
+}
