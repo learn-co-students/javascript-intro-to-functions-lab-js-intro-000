265ae869fa95244251f9cd18de55b071c2c44e49
James Calixte <jwend18@gmail.com>
2017-03-09 20:04:31 +0000

    Done.

--- index.js
+++ index.js
@@ -5,5 +5,22 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logshout(string) {
-  console.log(string.toUpperCase)
-}
+  return string.toUpperCase
+  console.console.log(sting);
+ }
+function sayHiToGrandma(string) {
+  if (string.toLowerCase) {
+    return "I can\'t hear you!";  }
+function sayHiToGrandma(string)   {
+  if (string.toUpperCase) {
+    return "YES INDEED!";}
+  }
+  function sayHiToGrandma(string) {
+    if (string.toLowerCase) {
+      return "I can\'t hear you!";
+    } else if (string.toUpperCase) {
+      return "YES INDEED!";
+    } else if ("I love you, Grandma.") {
+      return "I love you, too."
+    }
+  }
