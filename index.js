37f0072252107988fe3b36a56ca01738be6731bb
James Calixte <jwend18@gmail.com>
2017-03-10 02:53:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 function shout(string) {
-  return string.toUpperCase ()
-}
-function whisper(string) {
-  return string.toLowerCase()
-}
-function logshout(string) {
-  return string.toUpperCase
-  console.console.log(sting);
- }
-function sayHiToGrandma(string) {
-  if (string.toLowerCase) {
-    return "I can\'t hear you!";  }
-function sayHiToGrandma(string)   {
-  if (string.toUpperCase) {
-    return "YES INDEED!";}
-  }
-  function sayHiToGrandma(string) {
-    if (string.toLowerCase) {
-      return "I can\'t hear you!";
-    } else if (string.toUpperCase) {
-      return "YES INDEED!";
-    } else if ("I love you, Grandma.") {
-      return "I love you, too."
-    }
+   return string.toUpperCase()
+ };
+ 
+ function whisper(string) {
+   return string.toLowerCase()
+ };
+ 
+ function logShout(string) {
+   console.log(string.toUpperCase())
+ };
+ 
+ function logWhisper(string) {
+   console.log(string.toLowerCase())
+ };
+ 
+ function sayHiToGrandma(string) {
+   if (string.toLowerCase() === string) {
+     return "I can\'t hear you!";
+    } else if (string.toUpperCase() === string) {
+     return "YES INDEED!";
+   } else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
   }
