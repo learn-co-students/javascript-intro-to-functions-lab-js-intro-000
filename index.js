30afbadcc86067db86ddbdce544df3707751a541
Jason Yoon <jyoon1989@hotmail.com>
2017-03-19 03:33:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function shout(string) {
+  return string.toLowerCase()
+}
+function shout(string) {
+  console.log(string.toLowerCase())
+}
