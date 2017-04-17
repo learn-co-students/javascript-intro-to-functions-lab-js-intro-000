1c093347e2019133760f946068b74f5a5cd9fe06
Martine Stern <perri614@gmail.com>
2017-04-17 03:01:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+  return 'hello'.toUpperCase('HELLO')
+}
+
+function whisper(string) {
+  return 'HELLO'.toLowerCase('hello')
+}
+
+function logShout(string) {
+  console.log('HELLO')
+ }
+
+function logWhisper(string) {
+  console.log('hello')
+}
+
+function sayHiToGrandma(string) {
+  switch (string) {
+  case 'hello':
+  return ('I can\'t hear you!');
+  break;
+  case 'HELLO':
+  return ('YES INDEED!');
+  break;
+  default: 'I love you, Grandma.';
+  return ('I love you, too.')
+}}
