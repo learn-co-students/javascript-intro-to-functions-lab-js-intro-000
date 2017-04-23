77355f3d47a2cb6d8b30ce9888fe26c14aa4c7d3
Belén Renosto <belen-renosto@hotmail.com>
2017-04-23 23:16:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,5 @@
+function shout(string){return 'hello'.toUpperCase()}
+function whisper(string){return string.toLowerCase()}
+function logShout(string){console.log()string.toUpperCase()}
+function logWhisper(string){console.log()string.toLowerCase()}
+function sayHiToGrandma(string){if(lowercase.toLowerCase()){return "I can\'t hear you!"}}
