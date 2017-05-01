b8d2276e1cf04c06e315764ec53b96214ef59dd7
Folu Obatusin <nisutabo@gmail.com>
2017-05-01 00:40:07 +0000

    Done.

--- index.js
+++ index.js
@@ -14,3 +14,16 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string) {
+
+  if (lowercase.toLowerCase() === lowercase) {
+    return 'I can\'t hear you!'
+  }
+  else if (uppercase.toUpperCase() === uppercase) {
+    return 'YES INDEED!'
+  }
+  else if (string === 'I love you, Grandma.') {
+    return 'I love you, too.'
+  }
+}
