99eab546fb903f056caf3d8cbd24821aa6df8249
Sonya Estrada <sestrad8@student.solano.edu>
2017-05-09 03:14:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string){
+  return string.toUpperCase();
+  'Hello!'.toUpperCase();// 'HELLO!'
+function whisper(string){
+  return string.toLowerCase();
+  'Hello!'.toLowerCase();//'hello'
+function logShout(string){
+  console.log .toUpperCase();
+  'Hello!'.toUpperCase();//'HELLO!'
+function logWhisper(string){
+  console.log .toLowerCase();
+  'Hello!'.toLowerCase();//'hello!'
+function sayHiToGrandma (string){
+  return string .toLowerCase();
+  'string'.toLowerCase();//'I cant hear you!'
+function sayHiToGrandma(string){
+  return string.toUpperCase();
+  'string'.toUpperCase();//'YES INDEED!'
+function sayHiToGrandma(string){
+  return 'I love you, Grandma.';
+  'I love you, Grandma.';//'I love you, too.'
+}
+}
+}
+}
+}
+}
