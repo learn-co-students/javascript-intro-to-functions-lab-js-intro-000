2166ae409cf80552094fa8eac75ae1dace988900
Sophie Chang <soapycee@icloud.com>
2017-05-14 21:48:00 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+
+function logShout(string) {
+
+  console.log(string.toUpperCase());
+}
+
+function sayHiToGrandma(string) {
+  if (string===toLowercase()) {
+
+
+  return "I can't hear you!"
+}
+}
