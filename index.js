dd80f932defa323bb430d5855badd6aaccdfb6f2
Elias Wambugu <palsewa@yahoo.com>
2017-06-25 16:31:20 +0000

    Done.

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string){
+   if (string === string.toLowerCase()){
+     var lowerCase = string.toLowerCase();
+     return ('I can\'t hear you!');
+   }
+   else if (string === string.toUpperCase()){
+     var upperCase = 'YES INDEED!';
+     return ('YES INDEED!');
+   }
+   else{
+     var mixedCase = 'I love you, too.';
+     return(mixedCase);
+   }
+ }
