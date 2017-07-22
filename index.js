3dc771722b32c15ed3e7d741fade53239b826dcf
Jm Robin Robin <johnmichaelrobin@gmail.com>
2017-07-22 03:29:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(str) {
+  return str.toUpperCase();
+}
+
+function logShout(str) {
+  console.log(str.toUpperCase());
+}
+
+function logWhisper(str) {
+  console.log(str.toLowerCase());
+}
+
+function whisper(str) {
+  return str.lowerCase();
+}
+
+function sayHiToGrandma(str) {
+  if(str.toLowerCase()) {
+    return "I can't hear you!";
+  }
+}
+
+function sayHiToGrandma(str) {
+  if(str.toUpperCase()) {
+    return "YES INDEED!";
+}
+
+function sayHiToGrandma(str) {
+  if(str === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
