aecb80273605ca1ed7e40e80c35ffb098b95c121
David Clelland <davidclelland6@gmail.com>
2017-11-27 21:17:09 +0000

    Done.

--- index.js
+++ index.js
@@ -14,12 +14,19 @@ function logWhisper(string){
   console.log('hello'); string.toLowerCase();
 }
 
-function sayHiToGrandma(string){
-  if (string == string.toLowerCase);
-return 'I can\'t hear you!';
-} 
-else if  (string == string.toUpperCase);
-return 'YES INDEED!';
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
 
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
 
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
+}
 
