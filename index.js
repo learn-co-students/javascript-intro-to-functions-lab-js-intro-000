5938d37ad15e47ae486665746776de98f771004a
David Clelland <davidclelland6@gmail.com>
2017-11-27 20:38:21 +0000

    Done.

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ function whisper(string){
   return string.toLowerCase();
 }
 
+function logShout(string){
+  console.log('HELLO'); string.toUpperCase();
+}
+
+function logWhisper(string){
+  console.log('hello'); string.toLowerCase();
+}
+
+function sayHiToGrandma(string){
+  if (string == string.toLowerCase);
+  return 'I can\'t hear you!';
+}
+
+
