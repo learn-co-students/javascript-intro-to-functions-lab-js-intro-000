28a1d7a4efa12409523ecf2e76157267e647c9e6
Gunu Shrestha <realgunu@gmail.com>
2017-12-02 01:26:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(hello){
+  return ('hello').toUpperCase('HELLO!')
+}
+function whisper(Hello){
+  return('Hello').toLowerCase('hello')
+}
+function logShout(HELLO){
+  console.log('HELLO')
+}
+function logWhisper(hello){
+  console.log('hello')
+}
+
+function sayHiToGrandma(string){
+  if(string.toLowerCase()=== string) {
+    return "I can't hear you!"
+  }
+  if(string.toUpperCase() === string){
+    return "YES INDEED!"
+  }
+  if(string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
