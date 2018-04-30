d40bb616331d114e305f39061aca0d66c6619727
rgossomjr@gmail.com <rgossomjr@gmail.com>
2018-04-30 06:45:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(string) {
+  return string.toUpperCase(string)
+}
+
+function whisper(string){
+  return string.toLowerCase(string)
+}
+
+function logShout(string){
+console.log ("HELLO")
+}
+
+function logWhisper(string){
+console.log ("hello")
+}
+
+function sayHiToGrandma(string){
+  return ("I can\'t hear you!").toLowerCase("I can\'t hear you!")
+}
