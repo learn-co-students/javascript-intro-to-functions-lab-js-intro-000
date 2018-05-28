dfe29de6523fd0d256426763518e72d7daf11567
Carlos R <demo.ioverflow@gmail.com>
2018-05-28 18:09:31 +0000

    Done.

--- index.js
+++ index.js
@@ -17,11 +17,14 @@ function logWhisper(string){
 
 function sayHiToGrandma(string) {
 
-   if (string === string.toLowerCase()){
-    return "I can\'t hear you!";
-  } else if (string === string.toUpperCase()) {
-    return "YES INDEED!";
-  } else if (string === "I love you, Grandma.") {
-    return "I love you, too.";
+   if (string.toLowerCase() === string){
+    return "I can\'t hear you!"
   }
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+  return "Are you eating enough?"
 }
