6d04ac50b8ba0a7a4e9c91bc5a348227fc53ad8b
Hiza Mchaina <hmchaina@gmail.com>
2018-06-29 23:44:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-function shout(string) {
-  return string.toUpperCase()
+var uppercase, lowercase
+
+function shout(string){
+  uppercase = string.toUpperCase();
+  string = uppercase;
+  return uppercase;
 }
-
-function whisper(string) {
-  return string.toLowerCase()
+function whisper(string){
+  lowercase = string.toLowerCase();
+  string = lowercase;
+  return lowercase;
 }
-
-
-function logShout(string) {
-  console.log() string.toUpperCase
+function logShout(string){
+  uppercase = string.toUpperCase()
+  string = uppercase;
+  console.log(uppercase);
 }
-
-
-function logWhisper(string) {
-    console.log() string.toLowerCase
+function logWhisper(string){
+  lowercase = string.toLowerCase()
+  string = uppercase;
+  console.log(lowercase);
 }
-
-
-function sayHiToGrandma(string) {
-  if ((string)===uppercase) {
-    return 'YES INDEED!'
-  }
-  else if ((string)===lowercase) {
-    return "I can't hear you!"
-  }
-  else if ((string)==="I love you, Grandma.")) {
-    return "I love you, too."
-  }
-  }
+function sayHiToGrandma(string){
+    if (string.toLowerCase() === string){
+      return 'I can\'t hear you!';
+    }
+    if (string.toUpperCase() === string){
+      return 'YES INDEED!';
+    }
+    if (string === 'I love you, Grandma.'){
+      return 'I love you, too.';
+    }
 }
