1ed3300022a9f011b8193a373529dbb222b39101
Christa Gabert <gabert.cm@gmail.com>
2018-08-01 23:14:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function shout(string) {
   return string.toUpperCase()
 }
+
+function whisper(string) {
+  return whisper.toLowerCase()
+}
+
+function logShout(string) {
+  console.log.toUpperCase()
+}
+
+function logWhisper(string) {
+  console.log.toLowerCase()
+}
