70aeb5d752ebcd8dfda486b64623ca72e330f23f
Christa Gabert <gabert.cm@gmail.com>
2018-08-02 15:18:13 +0000

    Done.

--- index.js
+++ index.js
@@ -15,20 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase()) {
+  if ('string'.toLowerCase()) {
     return 'I can\'t hear you!'
-  }
-}
-
-function sayHiToGrandma(string) {
-  if (string.toUpperCase()) {
-    return 'YES INDEED!'
-  }
-}
-
-// passed #7
-function sayHiToGrandma(string) {
-  if (string === "I love you, Grandma.") {
-    return "I love you, too."
+    } else if ('string'.toUpperCase()) {
+        return 'YES INDEED!'
+      } else if (string === "I love you, Grandma.") {
+            return "I love you, too."
   }
 }
