778493d00c79eb8edd4f8102515b80be7ba29d45
Paul Elion <paul.j.elion@gmail.com>
2018-08-06 21:10:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-function shout(string) {
+function shout(string){
   return string.toUpperCase()
 }
 
-function whisper(string) {
+function whisper(string){
   return string.toLowerCase()
 }
 
-function logShout(string) {
+function logShout(string){
   console.log(string.toUpperCase())
 }
 
-function logWhisper(string) {
+function logWhisper(string){
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string) {
-  if (string === string.toUpperCase()) {
-    return "YES INDEED!"
-  } else if (string === string.toLowerCase()) {
+function sayHiToGrandma(string){
+  if (string === string.toLowerCase()){
     return "I can't hear you!"
-  } else {
+  } else if (string === string.toUpperCase()){
+    return "YES INDEED!"
+  } else if (string === "I love you, Grandma."){
     return "I love you, too."
   }
 }
