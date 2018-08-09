ff5977a7db73731cc97e9a8ed14780a37f860c2e
Simon Kiflay <szkiflay@gmail.com>
2018-08-09 14:43:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 function shout(string){
   return string.toUpperCase();
 }
@@ -33,3 +34,34 @@ function sayHiToGrandma(string){
 }*/
 
 
+=======
+function shout(String){
+  String.toUpperCase();
+}
+
+
+function whisper(String){
+  String.toLowerCase();
+}
+
+function logShout(String){
+  String.toLowerCase();
+  console.log(String);
+}
+
+function logWhisper(String){
+  String.toLowerCase();
+}
+
+function sayHiToGrandma(String){
+  String.toLowerCase();
+  console.log("I can't hear you!");
+}
+
+function sayHiToGrandma(String){
+  String.toUpperCase();
+  console.log("Yes Indeed!");
+  console.log("I love you, Grandma.");
+  console.log("I love you, too.");
+}
+>>>>>>> 7f4346e9c07f9f07360812f535023ff2216d3f4f
