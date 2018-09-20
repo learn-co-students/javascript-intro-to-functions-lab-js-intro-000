426305f20fc834b103ad55047062c4fa594f7846
Mary Jane Phifer <mjphifer@gmail.com>
2018-09-20 23:38:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 function shout(string) {
   return string.toUpperCase ()
 }
+
 function whisper(string) {
   return string.toLowerCase ()
 }
+
 function logShout(string) {
-  console.log('HELLO')
+  console.log(string.toUpperCase())
 }
+
 function logWhisper(string) {
-  console.log('hello')
+  console.log(string.toLowerCase())
 }
+
 function sayHiToGrandma(string) {
-  if (`string` === lowercase.toLowerCase) {
+  if (string.toLowerCase () === string) {
     return "I can't hear you!"
 }
-function sayHiToGrandma(string) {
-  if (`string`=== uppercase.toUpperCase) {
+  if (string.toUpperCase () === string) {
     return "YES INDEED!"
 }
-function sayHiToGrandma(string) {
-  if (`string`=== "I love you, Grandma."){
+  if (string === "I love you, Grandma.") {
     return "I love you, too."
+}
+
+return "Are you eating enough?"
 }
