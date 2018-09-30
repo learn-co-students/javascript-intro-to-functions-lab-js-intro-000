22e81d72f59701144ecd82419a057dc92c592902
Linda Ng <lindan121@yahoo.com>
2018-09-30 15:10:52 +0000

    Done.

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string===string.toLowerCase()){
     return ("I can\'t hear you!")}
-  if (string===string.toUpperCase()){
+  else if (string===string.toUpperCase()){
     return ("YES INDEED!")}
   else {
     return ("I love you, too.")
