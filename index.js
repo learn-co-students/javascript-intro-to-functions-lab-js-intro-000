d49ec4adac3a95fa6e5905b51ffc095cb7b21662
Pnina Breslav <pcrbus123@gmail.com>
2018-10-05 10:38:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,7 @@
+function shout(string) {
+  return string.toUpperCase()}
+function whisper(string){return string.toLowerCase()}
+function shout(string) {
+function sayHiToGrandma(string) {if (string.tolowercase()) {return("I can't hear you!"}
+ else if(string.toUpperCasecase()) {return("YES INDEED")
+}
