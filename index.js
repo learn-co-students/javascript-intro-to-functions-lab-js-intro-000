5377752cbb783013c433b98abbbe07078dfb1684
Hal Dunn <hallie.dunn1@gmail.com>
2018-10-15 01:09:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(string){
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logShout(string){ 
+  console.log (`HELLO`)
+const spyOn='HELLO'
+}
+function logWhisper(string){
+  console.log (`hello`)
+  const spyOn = `hello`
+}
+function sayHiToGrandma(string){
+ switch (string) { 
+   case (`I love you, Grandma.`): return (`I love you, too.`);
+ case string.toLowerCase(): return `I can\'t hear you!`;
+  case string.toUpperCase(): return`YES INDEED!`;
+}}
