a7aa2003509b6700b94d8a921ad92c02386ddc77
Carl Steele <casteeleu@gmail.com>
2018-11-19 04:07:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+function shout(string){
+  return string.toUpperCase(); 
+}
+
+function whisper(str){ 
+return str.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+  
+} 
+
+function logWhisper(string){
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string){
+  let lowercase = 'hello'
+  if (lowercase === string.toLowerCase()) {
+  return "I can't hear you!"
+  }
+} 
+
+function sayHiToGrandma(string){
+  let uppercase = 'HELLO'
+  if (uppercase === string.toUpperCase()) {
+  return "YES INDEED!"
+  }
+} 
+
+function sayHiToGrandma(string){
+let greeting = 'I love you, grandma'
+if (greeting === ){
+  return 'I love you, too'
+}
+}
+
+
+
