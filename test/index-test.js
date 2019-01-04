2de3c8dc7a8f0287ef901326387b057b931109af
mukhtar mohamed <mukhtarmohamed2790@gmail.com>
2019-01-04 19:06:54 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -44,12 +44,25 @@ describe('sayHiToGrandma(string)', function() {
   it('returns "I can\'t hear you!" if `string` is lowercase', function() {
     expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
   })
+  function sayHiToGrandma(string){
+    if(string.lowercase()){
+      return "i can't hear you!"
+    }
+  }
 
   it('returns "YES INDEED!" if `string` is uppercase', function() {
     expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
   })
+  esle if(string.uppercase()){
+    return "YES INDEED!"
+  }
 
   it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
     expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+
+else {
+  return "i love you too."
+}
+
   })
 })
