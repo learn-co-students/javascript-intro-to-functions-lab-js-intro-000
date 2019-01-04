97c5a5348aeea2562f4c2b1d37d2f2c044cf7d64
mukhtar mohamed <mukhtarmohamed2790@gmail.com>
2019-01-04 18:52:37 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -4,11 +4,16 @@ describe('shout(string)', function() {
     expect(shout('hello')).toEqual('HELLO')
   })
 })
-
+function shout(string){
+  return string.toUpperCase()
+}
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
     expect(whisper('HELLO')).toEqual('hello')
   })
+  function whisper(string){
+    return string.toLowerCase()
+  }
 })
 
 describe('logShout(string)', function() {
