bdec5f4910b1dc3f624920338664bb818b0e5968
Senai Mogos <smogos797@gmail.com>
2019-01-08 05:32:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log('hello'.toUpperCase())
+  return logShout
+}
+
+function sayHiToGrandma(string) {
+  console.log('HELLO'.toLowerCase()) = console.log("I can\'t hear you!")
+  console.log('hello'.toUpperCase()) = console.log("YES INDEED!")
+  console.log("I love you, Grandma.") = console.log("I love you, too.")
+  return sayHiToGrandma
+}
+
+function logWhisper(string) {
+  console.log('HELLO'.toLowerCase())
+  return logWhisper
+}
