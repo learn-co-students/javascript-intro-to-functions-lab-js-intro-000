c88a7a6c7914489b9b592adc563879a04f108662
rihan <dikshabelwal0@gmail.com>
2019-01-30 07:43:19 +0000

    Done.

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function sayHiToGrandma(string){
   else if(string==="I love you, Grandma."){
     return "I love you, too."
   }
-  else if(string.toUpperCase()===string){
+  else if(string.toUpperCase()==string){
     return "YES INDEED!"
   }
 }
