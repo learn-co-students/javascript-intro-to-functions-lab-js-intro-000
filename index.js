1cde6f018962ca39ff6306076e098f6c95fa00dc
Emir Cajic <emircajic@yahoo.com>
2019-02-21 22:15:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,44 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+
+function logShout(string) {
+
+  var test=string.toUpperCase();
+  console.log(test);
+}
+
+function logWhisper(string) {
+
+  var test=string.toLowerCase();
+  console.log(test);
+}
+
+function sayHiToGrandma(string){
+
+var lowercase='hello';
+var uppercase='HELLO';
+
+
+if(string==='I love you, Grandma.'){
+
+  return "I love you, too.";
+}
+
+if(string===lowercase){
+  return "I can't hear you!";
+}
+
+if(string===uppercase){
+  return "YES INDEED!";
+
+
+
+}
+
+}
