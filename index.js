55bde9d7894f5aaf65379391581faa9cc21fe024
Tess Guenthner <tessmadeleine@hotmail.com>
2019-02-23 17:29:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  if('hello') {console.log('HELLO')}
+}
+function logWhisper(string) {
+  if('HELLO') {console.log('hello')}
+}
+function sayHiToGrandma(string) {
+  if (string === "hello") {
+    return "I can't hear you!"}
+    else if (string === "HELLO") {
+      return "YES INDEED!"
+    }
+    else if (string === "I love you, Grandma.") {
+      return "I love you, too." }
+}
