cdca331cdb2889f5c93363223ff98ca86a8a3960
Reed G <ellisonrg@gmail.com>
2019-02-28 19:18:27 +0000

    Done.

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string){
-    if ('hello' === "lowercase"){
+    if (string.toLowerCase() === string){
       return "I can\'t hear you!"
-} else if ("YES INDEED!" === "uppercase"){
+} else if (string.toUpperCase() === string){
       return "YES INDEED!"
 } else if (string === "I love you, Grandma."){
       return "I love you, too."
