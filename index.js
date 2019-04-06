4938fcdb4bedefecbd37bce76974dc4c4801969c
Dianne Rees <drees3271@verizon.net>
2019-04-06 11:51:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,10 @@
+function shout(string){return string.toUpperCase()};
+function whisper(string){return string.toLowerCase()};
+function logShout(string){console.log(string.toUpperCase())};
+function logWhisper(string){console.log(string.toLowerCase())};
+var response = "I love you Grandma"
+function sayHiToGrandma (string.mixedCase()) {
+if (string === mixedcase)
+{
+  return "I love you,Grandma"}
+}
