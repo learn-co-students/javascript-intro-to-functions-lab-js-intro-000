ab7742c125241b28264982208c7e549844f08cb1
Giovanny <giovannydiaz98@yahoo.com>
2019-04-22 19:24:53 +0000

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ function shout(string) {
   return string.toUpperCase()
 }
 
-function whispers(string) {
+function whisper(string) {
   return string.toLowerCase()
+}
+
+ function logShout(string) {
+console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if ((string)===string.toUpperCase()) {
+    return "YES INDEED!"
+  }
+  else if ((string)===string.toLowerCase()) {
+    return "I can't hear you!"
+  }
+  else if ((string)==="I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
