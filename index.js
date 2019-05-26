bc5e7f10a3e107b12a1ab4d9aeb72f6af5196612
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-05-26 17:02:00 +0000

    Done.

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string == "hello") {
-    return "I can't hear you!"
+    return "I can't hear you!";
   }
   else if (string == "HELLO") {
-    return "YES INDEED!"
+    return "YES INDEED!";
   }
-  else if (string == "I love you, Grandma") {
-    return "I love you, too."
+  else if (string == "I love you, Grandma.") {
+    return "I love you, too.";
   }
 }
