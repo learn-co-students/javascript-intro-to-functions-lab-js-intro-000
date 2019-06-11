ccd9f4eee59cb62b5d3a632b3c7b40e3ddaf65b5
Justin Nowicki <justinnowicki@gmail.com>
2019-06-11 17:11:39 +0000

    Done.

--- index.js
+++ index.js
@@ -15,19 +15,19 @@ function logWhisper(string) {
 )}
 
 function sayHiToGrandma(string) {
-  if(string.toLowerCase() === string) {
-    return "I can\'t hear you!"
+  if(string === string.toLowerCase()) {
+    return 'I can\'t hear you!'
   }
 }
 
 function sayHiToGrandma(string) {
-  if(string.toUpperCase() === string) {
-    return "YES INDEED!"
+  if(string === string.toUpperCase()) {
+    return 'YES INDEED!'
   }
 }
 
 function sayHiToGrandma(string) {
   if(string === 'I love you, Grandma.') {
-    return "I love you, too."
+    return 'I love you, too.'
   }
 }
