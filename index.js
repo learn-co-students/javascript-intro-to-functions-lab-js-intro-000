389a0fc70c6896edd097c044fdfdaf13c0808a8e
Justin Nowicki <justinnowicki@gmail.com>
2019-06-15 22:23:06 +0000

    Done.

--- index.js
+++ index.js
@@ -18,15 +18,11 @@ function sayHiToGrandma(string) {
   if(string === string.toLowerCase()) {
     return 'I can\'t hear you!'
   }
-}
 
-function sayHiToGrandma(string) {
   if(string === string.toUpperCase()) {
     return 'YES INDEED!'
   }
-}
 
-function sayHiToGrandma(string) {
   if(string === 'I love you, Grandma.') {
     return 'I love you, too.'
   }
