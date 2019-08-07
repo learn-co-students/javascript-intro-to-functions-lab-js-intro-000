39fa8d79f81a01aa99876328601167e3a87a5dee
Nick Spera <nickaspera@gmail.com>
2019-08-07 01:39:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logShout(string){
+  console.log('HELLO')
+}
+function logWhisper(string){
+  console.log('hello')
+}
+
+/* save this, do i need to write these 3 conditions in the same function?
+
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma.")
+  then ("I love you, too."),
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase)
+  console.log ('I can\'t hear you!')
+}
+*/
+
+function sayHiToGrandma(string) {
+  if (string.toUpperCase)
+  return ("YES INDEED!")
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase)
+  return ("I can\'t hear you!")
+}
+
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma.")
+  return ("I love you, too.");
+}
