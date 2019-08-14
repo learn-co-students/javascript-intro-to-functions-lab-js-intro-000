bd68ce3d6980dea9c5a2acac62ecff5ce2220086
immutable-illustrator-3998 <tatismolin@gmail.com>
2019-08-14 20:58:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma (string) {
+  var lowercase = "I can't hear you!";
+  var uppercase = "YES INDEED!";
+  var other = "I love you, too.";
+  if (string.toLowerCase(string) === string) {
+    return lowercase;
+  }
+  else if (string.toUpperCase(string) === string) {
+    return uppercase;
+  }
+  else if ("I love you, Grandma." === string) {
+    return other
+  }
+}
