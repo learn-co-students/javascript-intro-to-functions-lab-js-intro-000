1c99f2f266d28d170cbce3f8c0eba9435e3d1eae
Emma Scott <ejms236@gmail.com>
2019-09-02 11:51:17 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function shout(string) {
 function whisper (string) {
   return string.toLowerCase()
 }
-function logShout(string)
-  console.log.restore string.toUpperCase()
+function logShout(string. toUpperCase)
+    console.log(logShout())
 }
