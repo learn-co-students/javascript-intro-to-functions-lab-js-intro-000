03833078115206f313fb3b538f05f6b90925ee9f
Jake Rose <jake1020@mac.com>
2019-10-04 02:09:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(toUpperCase)
+}
+
+function logWhisper(string) {
+  console.log(toLowerCase)
+}
+
+function sayHiToGrandma(string) {
+  return string.toLowerCase("I can't hear you!".toLowerCase)
+}
+
+function sayHiToGrandma(string) {
+  return string.toUpperCase("YES INDEED!".toUpperCase)
+}
+
+function shout(string) {
+  return string.toUpperCase()
+}
