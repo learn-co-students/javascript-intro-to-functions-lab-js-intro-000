924a734555f4ef20a2e00e9352328bf2529970e9
Henry Meehan <henryp.meehan@gmail.com>
2019-10-09 21:50:30 +0000

    Done.

--- index.js
+++ index.js
@@ -15,11 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-if (string === "I love you, Grandma.".toLowerCase()) {
+if (string === string.toLowerCase()) {
   return 'I can\'t hear you!'
-} else if (string === 'I love you, Grandma.'.toUpperCase()) {
-return 'YES INDEED!')
+} else if (string === string.toUpperCase()) {
+return 'YES INDEED!'
 } else if (string === "I love you, Grandma.") {
   return "I love you, too." }
-
 }
