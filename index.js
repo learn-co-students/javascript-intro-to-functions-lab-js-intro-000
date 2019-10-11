123676b49d974afd0f271c0ecb8d3e844f8f43c2
David Cha <david.cha44@qmail.cuny.edu>
2019-10-11 23:15:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  var cantHear = "I can\'t hear you!";
+    var canHear = "YES INDEED!";
+      var loveYou = "I love you, too.";
+
+          if(string.toLowerCase(string) === string){
+          return cantHear;
+          }
+            else if(string.toUpperCase(string) === string){
+              return canHear;
+              }
+                else if("I love you, Grandma." === string) {
+                  return loveYou;
+                  }
+}
