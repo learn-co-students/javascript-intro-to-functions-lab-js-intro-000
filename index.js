c0737b7d7fe378211e28d50f641c51116d6e5edf
Fredj Khlifi <fkhlifi@gmail.com>
2019-10-29 23:42:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function whisper(string) {
+  let tring = "Hello!"
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function logWhisper(string)  {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  let lowercase = "hello!";
+  if (lowercase.toLowerCase() === lowercase ) {
+    return "I can\'t hear you!";
+  }
+}
+
+function sayHiToGrandma(string) {
+
+}
+
+function sayHiToGrandma(string) {
+  if(string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
