4e6f890d2b0883e1c3685629ce772252ed8a766c
Joe Perna <joe.m.perna@gmail.com>
2019-11-07 03:09:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,53 @@
+
+function shout(string){
+
+  return string.toUpperCase()
+}
+
+function whisper(string){
+
+return string.toLowerCase()
+
+}
+
+function logShout(string){
+
+console.log(string.toUpperCase());
+
+}
+
+
+function logWhisper(string){
+
+console.log(string.toLowerCase())
+
+}
+
+
+
+
+function sayHiToGrandma(string){
+
+string.toLowerCase()
+
+if (string.toLowerCase() === string){
+
+  return 'I can\'t hear you!'
+}
+
+string.toUpperCase()
+
+if (string.toUpperCase() === string){
+
+return "YES INDEED!"
+
+}
+
+string = "I love you, Grandma."
+
+if (string === "I love you, Grandma."){
+
+  return "I love you, too."
+}
+
+}
