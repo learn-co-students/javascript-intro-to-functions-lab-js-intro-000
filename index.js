381dbfa90f77c536d5eba0691fb6e3a9bbaff1a5
Taulant Ibraj <tibraj94@gmail.com>
2019-12-26 14:59:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+  return string.toUpperCase("Hello!")
+}
+function whisper(string) {
+  return string.toLowerCase("Listen")
+}
+function logShout(string) {
+  console.log string.toUpperCase("Today!")
+}
+function logWhisper(string) {
+  console.log string.toLowerCase("Now")
+}
+function sayHiToGrandma(string) {
+  return string.toLowerCase("I can't hear you!")
+}
+function sayHiToGrandma(string) {
+  return string.toUpperCase("YES INDEED")
+}
+function sayHiToGrandma(string) {
+  if("I love you, Grandma.");
+  return("I love you, too.")
+}
