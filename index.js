3fbb8bc85dcaa4e9030337aeaa917a6617ecaf17
Stephanie Lella <stelella2007@gmail.com>
2019-12-30 05:04:37 +0000

    Done.

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ function shout(string) {return string.toUpperCase()}
 function whisper(string) {return string.toLowerCase()}
 
 function logShout(string) {console.log(string.toUpperCase())}
-function logWhisper(string) {console.log(string.lowercase())}
+function logWhisper(string) {console.log(string.toLowerCase())}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) { return "I can't hear you!"}
+}
