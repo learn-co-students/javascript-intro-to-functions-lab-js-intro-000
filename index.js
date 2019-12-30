3ffccd610b885624f8847ae1703323b45077d6a6
Stephanie Lella <stelella2007@gmail.com>
2019-12-30 05:27:48 +0000

    Done.

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ function whisper(string) {return string.toLowerCase()}
 function logShout(string) {console.log(string.toUpperCase())}
 function logWhisper(string) {console.log(string.toLowerCase())}
 
-function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) { return "I can't hear you!"}
-}
-  if (string.toUpperCase() === string) { return "YES INDEED!"}
-}
-if (string === "I love you, Grandma.") {
-  return "I love you too."
-}}
+
+function sayHiToGrandma(string){
+    if (string === string.toLowerCase()){
+      var lowerCase = string.toLowerCase();
+      return ('I can\'t hear you!');
+    }
+    else if (string === string.toUpperCase()){
+      var upperCase = 'YES INDEED!';
+      return ('YES INDEED!');
+      }
+      else{
+        var mixedCase = 'I love you, too.';
+        return(mixedCase);
+      }
+    }
