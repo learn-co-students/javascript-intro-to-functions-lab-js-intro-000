f70179f4f868dcde42b57c904995778d23e68938
James Stamps Jr <jstampshere@gmail.com>
2020-01-26 19:50:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log  (string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log (string.toLowerCase())
+}
+
+lowercase.toLowerCase(string) === lowercase
+function sayHiToGrandma(string) {
+  return ("I can\'t hear you!")
+}
+
+uppercase.toUpperCase(string) === uppercase
+function sayHiToGrandma(string) {
+  return ("YES INDEED!")
+}
+
+function sayHiToGrandma("I love you, Grandma") {
+  return "I love you, too."
+}
