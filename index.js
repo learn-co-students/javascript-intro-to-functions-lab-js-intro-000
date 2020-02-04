afa78029083d64674623165e5cb6ab73f3f301aa
Roberta Munive <ramunive@gmail.com>
2020-02-04 16:29:51 +0000

    Done.

--- index.js
+++ index.js
@@ -21,7 +21,11 @@ function sayHiToGrandma(string){
 
   if (string.toLowerCase() === string){
     return "I can't hear you!";
-  } else if (string.toUpperCase() === string) {
+  }
+
+  if (string.toUpperCase() === string) {
     return "YES INDEED!"
   }
+
+  return "Stay and visit awhile."
 }
