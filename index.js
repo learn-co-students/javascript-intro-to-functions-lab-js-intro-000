3ea29931f812ed62b087551a4a95b290ea21dcd6
loyal-transpiler-0513 <ifeawoyale@yahoo.com>
2020-02-07 00:05:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout('hello') {
+  console.log(string.toUpperCase);
+}
+
+function logWhisper('hello') {
+  console.log(string.toLowerCase);
+}
