75b5b6f78be0a133cc2541926b4b832276121540
mark mosby <markemosby@msn.com>
2020-02-11 23:50:03 +0000

    Done.

--- index.js
+++ index.js
@@ -16,3 +16,14 @@ function logWhisper(string) {
   string = lowercase;
   console.log(string);
 }
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string)
+  {
+      return 'I can\'t hear you!';
+}
+if (string.toUpperCase() === string){
+      return 'YES INDEED!';
+    }
+    if (string === 'I love you, Grandma.'){
+      return 'I love you, too.';
+    }}
