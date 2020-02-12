755449475bde47a9f9631ecd2e740510d247db7b
Liridona Morina <liridonamorina00@gmail.com>
2020-02-12 17:31:34 +0000

    Done.

--- index.js
+++ index.js
@@ -22,8 +22,9 @@ function sayHiToGrandma(string){
     {
       return "YES INDEED!"
     }
-  else if(string === "I love you,Gandma.")
+  else if(string === "I love you, Gandma.")
     {
-      return "I love you,too.";
+
+      return "I love you, too.";
     }
 }
