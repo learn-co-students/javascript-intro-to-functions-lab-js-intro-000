4dc3feef6804b5bbc153603748eacb2e9aebf98f
Antonia Gavrilis <antoniazabel@gmail.com>
2020-03-06 17:33:38 +0000

    Done.

--- index.js
+++ index.js
@@ -21,3 +21,14 @@ function sayHiToGrandma(string) {
     return "I love you, too."
   }
 }
+  function sayHiToGrandma(string){
+      if (string == string.toLowerCase()){
+    return "I can't hear you!"
+  }
+      if (string == string.toUpperCase()){
+    return "YES INDEED!"
+  }
+      if (string == "I love you, Grandma."){
+    return "I love you, too."
+}
+  }
