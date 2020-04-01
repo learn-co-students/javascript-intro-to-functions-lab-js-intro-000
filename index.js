a33c15f4e22529ad0ca2b49d7557c5f340285bee
Robert Daub <rjd2066@gmail.com>
2020-04-01 17:10:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string) {
+ return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  var uppercase = "HELLO!"
+  uppercase.toUpperCase() === uppercase
+
+  var lowercase = "hello!"
+  lowercase.toLowerCase() === lowercase
+  if (string.toLowerCase())
+    return "I can't hear you!"
+
+  else if (string.toUpperCase())
+    return "YES INDEED!"
+
+  else (string("I love you, Grandma."))
+    return "I love you, too."
+
+}
