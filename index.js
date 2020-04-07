1d43a4e68ea59d324164773580e14b95423b3643
Sean Espinosa <sean.espinosa1@gmail.com>
2020-04-07 03:34:36 +0000

    Done.

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string) {
   if (string === string.toUpperCase()) {
     return "YES INDEED!"
   }
-  if (string === string) {
+  if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
 }
