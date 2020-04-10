d8e31f27221a8c8058dbfe749e143b93010e0fcc
Joey Hendricks <joeysguitartabs@yahoo.com>
2020-04-10 22:19:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+	let lowercase = string;
+	let uppercase = string;
+  if(lowercase.toLowerCase() === lowercase) {
+    return "I can't hear you!";
+  } else if (uppercase.toUpperCase() === uppercase) {
+    return "YES INDEED!";
+  } else if (string === 'I love you, Grandma.') {
+    return 'I love you, too.';
+  } else {
+    return 'There was an error';
+  }
+}
