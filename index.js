0e4600fafda274e2abe557f5e7bbed48e9101d7e
Ruth Vera <rvamore89@gmail.com>
2020-05-01 00:23:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+  function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(hello) {
+ console.log(hello.toUpperCase())
+
+}
+
+function logWhisper(hello) {
+ console.log(hello.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase)
+  return ("I can't hear you!");
+
+
+
+}
