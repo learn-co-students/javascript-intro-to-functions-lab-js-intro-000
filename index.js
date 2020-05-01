56776ed3c8154ee3e7bc3dea0152400be04fb6d5
Ruth Vera <rvamore89@gmail.com>
2020-05-01 00:37:17 +0000

    Done.

--- index.js
+++ index.js
@@ -16,9 +16,12 @@ function logWhisper(hello) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase)
+  if (string === string.toLowerCase()) {
   return ("I can't hear you!");
-
-
+} else if (string === string.toUpperCase()) {
+  return ("YES INDEED!") }
+  else if (string === "I love you, Grandma.") {
+    return ("I love you, too.")
+  }
 
 }
