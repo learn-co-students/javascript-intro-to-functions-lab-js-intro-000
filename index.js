371fe07ae7a118020475a3ca0339ecf77ed5a1c4
Scarlett Baez <s.baez1010@gmail.com>
2020-05-31 00:16:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout('hello') {
+  return string.toUpperCase()
+}
+function whisper('HELLO') {
+return string.toLowerCase()
+}
+function logShout('HELLO') {
+console.log ('HELLO')
+}
+function logWhisper('hello') {
+console.log ('hello')
+}
+function sayHiToGrandma('I love you, Grandma.') {
+if (.toLowerCase) {
+  return ("I can't hear you!")
+}
+}
+if (.toUppercase) {
+return ('YES INDEED!')
+}
+if ('I love you, Grandma.') {
+return('I love you too.')
+}
