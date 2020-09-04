ebb5aca7f62dd2005da027114819c5ec2caaaf67
Cole Cezeaux <colecezeaux@gmail.com>
2020-09-04 14:44:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 function shout(string) {
-  return string.toUpperCase ()
+  return string.toUpperCase()
 }
+
 function whisper(string) {
   return string.toLowerCase()
 }
+
 function logShout(string) {
-  console.log (string.toUpperCase())
+  console.log(string.toUpperCase())
 }
+
 function logWhisper(string) {
-  console.log (string.toLowerCase())
+  console.log(string.toLowerCase())
 }
+
 function sayHiToGrandma(string) {
-  console.log ("THIS IS THE ARGUMENT.", string)
-if (string === string.toUpperCase()) { // if string is uppercase
+  if (string === string.toUpperCase()) { // if string is uppercase
   return "YES INDEED!" // return "YES INDEED!"
 
 } else if (string === string.toLowerCase()) { // if string is lowercase
@@ -20,4 +23,5 @@ if (string === string.toUpperCase()) { // if string is uppercase
 
 } else if (string === "I love you, Grandma.") {// if string equals "I love you, Grandma."
   return "I love you, too." // return "I love you, too."
-} }
+}
+}
