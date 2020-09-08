75b5dd40d42b51480dc8c1ac19f76e4cd366cba7
Alon G <greenberg.alon@gmail.com>
2020-09-08 21:57:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+
+//eceives one argument and returns it in all caps
+function shout (string) {
+  return string.toUpperCase();
+}
+
+// receives one argument and returns it in all lowercase
+function whisper (string) {
+  return string.toLowerCase();
+}
+
+//calls console.log() its one argument in all caps
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+//calls console.log() its one argument in all lowercase
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+//  sayHiToGrandma(string)
+//  5) returns "I can't hear you!" if `string` is lowercase
+//  6) returns "YES INDEED!" if `string` is uppercase
+//  7) returns "I love you, too." if `string` is "I love you, Grandma."`
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()){
+    return "I can't hear you!";
+  } else if (string === string.toUpperCase()){
+    return "YES INDEED!";
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
