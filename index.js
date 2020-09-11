bafa9ece7f7db3c857fce9fe62dcfc2e84be5cf5
Cole Cezeaux <colecezeaux@gmail.com>
2020-09-11 18:19:35 +0000

    Done.

--- index.js
+++ index.js
@@ -15,13 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.toUpperCase()) { // if string is uppercase
-  return "YES INDEED!" // return "YES INDEED!"
-
-} else if (string === string.toLowerCase()) { // if string is lowercase
-  return ("I can't hear you!") // return "I can't hear you!"
-
-} else if (string === "I love you, Grandma.") {// if string equals "I love you, Grandma."
-  return "I love you, too." // return "I love you, too."
-}
+  if(string === string.toLowerCase()) {
+    return "I can't hear you!"
+  } else if(string === string.toUpperCase()) {
+    return "YES INDEED!"
+  } else if(string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
