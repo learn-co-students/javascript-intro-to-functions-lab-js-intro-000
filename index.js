be4d82af4b7fd54926a7744de90d6537ae59ccf6
David Glading <davidjglading@gmail.com>
2020-09-27 17:16:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(string)  {
+  return string.toUpperCase()
+}
+function whisper(string)  {
+  return string.toLowerCase()
+}
+function logShout(string)  {
+  console.log string.toUpperCase()
+}
+function logWhisper(string)  {
+  console.log string.toLowercase()
+}
+function sayHiToGrandma(string)  {
+  console.log string.toLowerCase
+  return("I can't hear you")
+  console.log string.toUpperCase()
+  return("YES, INDEED!")
+  console.log string('I love you, Grandma')
+  return string(I love you, too)
+}
