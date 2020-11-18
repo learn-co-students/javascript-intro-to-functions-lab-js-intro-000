b8bb328e8555fd46da82a8ec735b4ff7434baa79
Adam B <adamisbarker@hotmail.com>
2020-11-18 00:39:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string){
+  return 'HELLO'.toLowerCase()
+}
+function logShout(hello){
+  console.log(hello.toUpperCase())
+}
+function logWhisper(HELLO){
+  console.log(HELLO.toLowerCase())
+}
+function sayHiToGrandma(string){
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!"
+  } if (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  } if (string = "I love you, Grandma."){
+    return "I love you, too."
+  }
+}
