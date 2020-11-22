4352ca4e046de045079f0b63757219cc00dc4d50
Adam Moore <adamjamesmoore@live.co.uk>
2020-11-22 19:49:34 +0000

    Done.

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ function sayHiToGrandma(string){
   if  (string.toLowerCase() === string){
   return ("I can't hear you!");
 }
-if (string.toUpperCase()){
+if (string.toUpperCase() === string) {
   return ("YES INDEED!");
 }
-if (string === ("I love you, Grandma")){
-  return ("I love you, too");
+if (string === ("I love you, Grandma.")){
+  return ("I love you, too.");
 }
 }
