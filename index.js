aeae3d948bb667082bf3bee638fac6d5f8532787
James <jamesalexandersmith2112@gmail.com>
2020-12-21 22:42:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+function whisper(string) {
+  return string.toLowerCase();
+} 
+function logShout(string) {
+  console.log (string.toUpperCase());
+}
+function logWhisper(string) {
+  console.log (string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+
+var uppercase = "HELLO!";
+uppercase.toUpperCase() === uppercase;
+var lowercase = 'hello!';
+lowercase.toLowerCase() === lowercase;
+var mixedCase = 'I love you, Grandma.';
+mixedCase.toLowerCase === mixedCase;
+
+mixedCase.toUpperCase === mixedCase;
+
+if (string === string.toUpperCase()) {
+  return "YES INDEED!"
+} else if (string === string.toLowerCase()) {
+  return "I can't hear you!"
+} else {
+  return "I love you, too."
+}
+}
