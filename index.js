ab18b54555e61f69dee98cce292b36ff83c9fd82
Lupita Rivera <lupitarivera8899@gmail.com>
2020-12-28 23:04:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+'Hello!'.toUpperCase() // 'HELLO!'
+function shout(string) {return string.toUpperCase()
+}
+
+'HELLO!'.toLowerCase() // 'hello'
+function whisper(string){return string.toLowerCase()
+}
+
+function logShout (string) {
+  console.log(string.toUpperCase( ));}
+
+function logWhisper (aWord) {
+  console.log(aWord.toLowerCase( ));}
+  function sayHiToGrandma (aWord) {
+    var cantUnswer = "I can't hear you!";
+    var yesUnswer = "YES INDEED!";
+    var lovUnswer = "I love you, too.";
+    if (aWord.toLowerCase(aWord) === aWord) {
+      return cantUnswer;
+    }
+    else if (aWord.toUpperCase(aWord) === aWord) {
+      return yesUnswer;
+    }
+    else if ("I love you, Grandma." === aWord) {
+      return lovUnswer
+    }
+  }
