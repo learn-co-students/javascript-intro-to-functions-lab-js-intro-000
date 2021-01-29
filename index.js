2b5dbfb0acf13f6f5c2db14c9f113ece6d94a562
Dondre Mclenan <d.mclenan@gmail.com>
2021-01-29 12:06:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string){
+return string.toLowerCase()
+}
+
+function logShout(string) {
+console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  return string.toLowerCase "I can't hear you!"
+  return string.toUpperCase "YES INDEED!"
+  return string.toLowerCase"I love you, too."
+  return (string.toLowerCase "I love you,Grandma."
+  }
