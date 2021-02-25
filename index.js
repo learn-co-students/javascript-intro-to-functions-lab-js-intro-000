eb028cfc3f480f0bb74c5d7998990215696fa2d9
Roxana Sulton <roxsulton@icloud.com>
2021-02-25 23:52:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+// 'Hello!'.toUpperCase()// 'HELLO'
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+// 'Hello!'.toLowerCase()// 'HELLO'
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+  // 'Hello'.toUpperCase()// 'HELLO'
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+// 'Hello!'.toLowerCase()// 'HELLO'
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+// 'hello'.toLowerCase() // 'I can't hear you!'
+
+function sayHiToGrandma(string) {
+  if (string == "hello") {
+  return "I can't hear you!"
+} else if (string == "HELLO") {
+  return "YES INDEED!"
+  } else if (string == "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
