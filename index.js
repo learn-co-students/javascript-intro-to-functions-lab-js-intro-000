3f2d6c8099973d5254d1cfa31512d9f1df5bb87c
Roxana Sulton <roxsulton@icloud.com>
2021-02-25 23:57:43 +0000

    Done.

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ function logWhisper(string) {
 // 'hello'.toLowerCase() // 'I can't hear you!'
 
 function sayHiToGrandma(string) {
-  if (string == "hello") {
-  return "I can't hear you!"
-} else if (string == "HELLO") {
-  return "YES INDEED!"
+  if (string.toLowerCase() == string) {
+    return "I can't hear you!"
+} else if (string.toUpperCase() == string) {
+    return "YES INDEED!"
   } else if (string == "I love you, Grandma.") {
     return "I love you, too."
   }
