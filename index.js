bbb5e0b4c04d08c0667eda7196de8e5a653f6809
Giovanni Rodriguez <ggabriel92@hotmail.com>
2021-03-17 08:40:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string)  {
+  return string.toUpperCase()
+}
+
+function whisper(string)  {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log("HELLO")
+}
+
+function logWhisper(string) {
+  console.log("hello")
+}
+
+var "HELLO!" === "I can't hear you!"
+function sayHiToGrandma(string) {
+  return string.toLowerCase("I can't hear you!")
+}
+ var "HELLO!" === "YES INDEED!"
+function sayHiToGrandma(string) {
+  return string.toUpperCase("YES INDEED!")
+}
+
+var "I love you, GRANDMA" === "I love you, too!"
+function sayHiToGrandma(string) {
+  return("I love you, too!")
+}
