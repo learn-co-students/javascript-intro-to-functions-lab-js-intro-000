2702ba8ee3c7cc646796af2bf2df0eeae962bb4f
George Fernandez <fernandezg704@gmail.com>
2021-03-30 05:46:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string) {
+   let string.toLowerCase()
+    return "I can't hear you!"
+    return "YES INDEED!"
+    return "I love you, too."
+}
