f51e8ccf6a2b5d7b7182f6151a5ed20a27cc0dff
bere abreo <berenicej2013@gmail.com>
2021-04-02 04:08:44 +0000

    Done.

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string) {
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+  } else if (string.toLowerCase() === string) {
+    return "I can't hear you!";
+  } else {
+    return "I love you, too."
+  }
+}
