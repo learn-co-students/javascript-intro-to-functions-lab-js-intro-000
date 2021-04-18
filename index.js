8632b9428644a46312067386ba927dea4316cc67
Moose <Davis>
2021-04-18 13:29:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-function should(string) {}
+function shout(string) {
+  return string.toUpperCase();
+}
 
-function whisper(should) {}
+function whisper(string) {
+  return string.toLowerCase();
+}
 
-function logShoult(string) {}
+function logShout(string) {
+  return console.log(string.toUpperCase());
+}
 
-function sayHiToGrandma(string) {}
+function logWhisper(string) {
+  return console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!";
+  }
+  if (string === string.toUpperCase()) {
+    return "YES INDEED!";
+  }
+  if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
