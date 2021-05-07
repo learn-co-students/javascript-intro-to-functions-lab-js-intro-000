bd45f1fb9dd5b1f3d1ec43ffeb56b371f9bda315
Gerald Dino <dino.gerald108@gmail.com>
2021-05-07 13:23:01 +0000

    Done.

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ function whisper(string) {
 /*
 function logShout(string) {
   console.log(string.toUpperCase());
-} */
+}
+*/
 
 function logShout(string) {
   console.log(shout(string));
@@ -18,13 +19,26 @@ function logShout(string) {
 /*
 function logWhisper(string) {
   console.log(string.toLowerCase());
-} */
+}
+*/
 
  function logWhisper(string) {
    console.log(whisper(string));
  }
 
+/*
  function sayHiToGrandma(string) {
+   if (string.toLowerCase() === string) {
+     return 'I can\'t hear you!';
+   } else if (string.toUpperCase() === string) {
+     return 'YES INDEED!';
+   } else if (string === 'I love you, Grandma.') {
+     return 'I love you, too.';
+   }
+ }
+*/
+
+  function sayHiToGrandma(string) {
    if (whisper(string) === string) {
      return 'I can\'t hear you!';
    } else if (shout(string) === string) {
