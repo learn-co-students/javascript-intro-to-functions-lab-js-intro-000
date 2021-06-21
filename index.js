38189dc4f610248e23d4719f9e2c03d0bd81a37f
Yearim Yi <leslieyi411@gmail.com>
2021-06-21 20:49:45 +0000

    Done.

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
   return "I can't hear you!"
-  } if (string === string.toUpperCase()) {
+  } else if (string === string.toUpperCase()) {
   return "YES INDEED!"
-  } if (string === "I love you, Grandma.") {
+  } else if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
   }
